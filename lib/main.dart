4fa8341ef83a352369cfe6b22bbb3bd585dc233d
arby ardiansyah <arbiardiansyah@gmail.com>
2024-10-01 09:30:48 +0700

    Menambahkan gambarr ke README

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,63 @@
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
-}
+import 'package:flutter/cupertino.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Hilangkan const dari variabel lokal
-    String appTitle = 'Flutter layout demo';
-
-    return MaterialApp(
-      title: appTitle,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(appTitle),
+    return const CupertinoApp(
+      title: 'Flutter layout demo',
+      theme: CupertinoThemeData(
+        brightness: Brightness.light,
+        primaryColor: CupertinoColors.systemBlue,
+      ),
+      home: CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          backgroundColor: CupertinoColors.systemGrey,
+          middle: Text('Flutter layout demo'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        // properti body digunakan untuk menampilkan konten halaman
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Hello World'),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+void main() => runApp(const MyApp());
+
+
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // Hilangkan const dari variabel lokal
+//     String appTitle = 'Flutter layout demo';
+
+//     return MaterialApp(
+//       title: appTitle,
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text(appTitle),
+//         ),
+//         body: const Center(
+//           child: Text('Hello World'),
+//         ),
+//       ),
+//     );
+//   }
+// }
